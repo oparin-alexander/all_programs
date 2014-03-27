8e29fe0d66c4230cb22f42fe3967b0a2cbcae0a7
oparin-alexander <oparin.mipt@yandex.ru>
2014-03-27 23:23:02 +0300

    Update my_cp.c

--- my_cp.c
+++ my_cp.c
@@ -7,15 +7,11 @@
 #include <stdlib.h>
 #include <string.h>  
 #include <stdio.h>
-#include <time.h>
-#include <pwd.h>
-#include <grp.h>
+
 
 #define NBYTE 1000
 #define MAX_PATH_LENGTH 255
 int r=0;
-char temp_str1[MAX_PATH_LENGTH];
-char temp_str2[MAX_PATH_LENGTH];
 
 int open_file(char* fileName, int mask, int mode)
 {
@@ -34,10 +30,8 @@ void my_copy(int fd1, int fd2)
 	
 	while( ( temp_n=read(fd1, buff, NBYTE) ) != 0 )
 	{	
-		if (temp_n<0) {
+		if (temp_n<0) 
 			perror("copy_error");
-			exit(1);
-		}
 		write(fd2, buff, temp_n);
 	}
 	
@@ -51,21 +45,20 @@ void error_exit(char* error_text)
 }
 
 //1-файл в файл, 2-что угодно в дир 
-int read_args(int argc, char **argv)
+int read_args(int argc, char **argv, int *k)
 {
 	if(argc<3) error_exit("incorrect input: few arguments");
 	
 	struct stat buf;
-	int begin;
+	int begin=1;
+	*k=1;
 	
 	if( strcmp(argv[1], "-r")==0 ) {
 		 r=1;
 		 begin=2;
 	 }
-	 else {
+	 else 
 		 r=0;
-		 begin=1;
-	 }
 	
 	if ( stat(argv[argc-1], &buf)<0) 
 	{
@@ -82,12 +75,12 @@ int read_args(int argc, char **argv)
 				error_exit("incorrect input: source path is dir and not found -r");
 		}
 		
-		return 2;
+		*k=2;
 	}
 	else if(r==1 || argc!=3) 
 		error_exit("incorrect input: for cp to file");
-	
-	return 1;
+		
+	return begin;
 }
 
 void copy_file(char* file1, char* file2)
@@ -121,27 +114,27 @@ void make_cp(char *from_path, char *to_path)
     while ( (curr_dir = readdir(dir)) != NULL) 
     {	
 		if (curr_dir->d_name[0]!='.') {
-			strcat(temp_str2, "/");
-			strcat(temp_str2, curr_dir->d_name); 			
-			strcat(temp_str1, "/");
-			strcat(temp_str1, curr_dir->d_name);	
+			strcat(to_path, "/");
+			strcat(to_path, curr_dir->d_name); 			
+			strcat(from_path, "/");
+			strcat(from_path, curr_dir->d_name);	
 			
-			if (stat(temp_str1, &buf) < 0)
+			if (stat(from_path, &buf) < 0)
 				perror(curr_dir->d_name);			
 			else if( S_ISDIR(buf.st_mode) ) {
-				if (stat(temp_str2, &buf1)<0)
-					if( mkdir(temp_str2, buf.st_mode) )
-						perror(temp_str2);
-				make_cp(temp_str1, temp_str2);
+				if (stat(to_path, &buf1)<0)
+					if( mkdir(to_path, buf.st_mode) )
+						perror(to_path);
+				make_cp(from_path, to_path);
 			}
 			else 
-				copy_file(temp_str1, temp_str2);
+				copy_file(from_path, to_path);
 			
 			int i;
-			for(i=strlen(temp_str1)-1; temp_str1[i]!='/' && i>0; --i) temp_str1[i]='\0';
-			temp_str1[i]='\0';
-			for(i=strlen(temp_str2)-1; temp_str2[i]!='/' && i>0; --i) temp_str2[i]='\0';
-			temp_str2[i]='\0';
+			for(i=strlen(from_path)-1; from_path[i]!='/' && i>0; --i) from_path[i]='\0';
+			from_path[i]='\0';
+			for(i=strlen(to_path)-1;  to_path[i]!='/' && i>0; --i) to_path[i]='\0';
+			to_path[i]='\0';
 		}
     }
     
@@ -149,43 +142,39 @@ void make_cp(char *from_path, char *to_path)
 	return;	
 }
 
-
-
 int main(int argc, char **argv)
 {
-	int k=read_args(argc, argv);
+	int k=0;
+	int begin=read_args(argc, argv, &k);
 	if (k==1)
 		copy_file(argv[argc-2], argv[argc-1]);
 	else if (k==2) {
-		int begin=0;
+		char temp_str1[MAX_PATH_LENGTH];
+		char temp_str2[MAX_PATH_LENGTH];
+
 		struct stat buf, buf1;
 		strcpy(temp_str2, argv[argc-1]);
-		if (r==1) begin=2; else begin=1;
 		
 		for(int i=begin; i<argc-1; ++i)
 		{
 			stat(argv[i], &buf);
 			strcpy(temp_str1, argv[i]);
-			int j=0;
-			char temp[50];
-			for(j=strlen(argv[i])-1; argv[i][j]!='/' && j>=0; --j);
-			for(int k=j+1; k<=strlen(argv[i]); ++k) 
-			{
-				temp[k-j-1]=argv[i][k]; 
-				temp[k-j]='\0'; 
-			}
 			strcpy(temp_str2, argv[argc-1]);
+			
+			int j=0;
+			for( j=strlen(argv[i])-1; argv[i][j]!='/' && j>=0; --j);
+			
 			strcat(temp_str2, "/");
-			strcat(temp_str2, temp);
+			strcat(temp_str2, &argv[i][j+1]);
 			
 			if( S_ISDIR(buf.st_mode) ) {
 				if (stat(temp_str2, &buf1)<0)
 					if( mkdir(temp_str2, buf.st_mode) )
 						perror(temp_str2);
-				make_cp (argv[i], temp_str2);
+				make_cp (temp_str1, temp_str2);
 			}
 			else
-				copy_file(argv[i], temp_str2);
+				copy_file (temp_str1, temp_str2);
 		}
 	}
 	exit(0);
